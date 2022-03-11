780218309cded3090f1d68a5a4915b4990fdb2a4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-10 19:40:27 -0700

    first commit

---
+++ PersonalBusinessCard/InfoVIew.swift
@@ -0,0 +1,20 @@
+//
+//  InfoVIew.swift
+//  PersonalBusinessCard
+//
+//  Created by Mitch Andrade on 2/23/22.
+//
+
+import SwiftUI
+
+struct InfoVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoVIew()
+    }
+}
